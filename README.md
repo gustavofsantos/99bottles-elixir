ca60f54e1f3ab88af2709ed18bb7bcba1695f5f7
Gustavo Santos <gustavofsantos@outlook.com>
2021-01-08 15:52:45 -0300

    added initial implementation

---
+++ README.md
@@ -0,0 +1,18 @@
+# 99Bottles
+
+Elixir implementation of default 99 Bottles of OOP exercise. 
+
+## What you need to run this
+
+* Elixir @^1.4
+* mix
+
+## Running the tests
+
+You can run all of them using the `test` or `test.watch` mix tasks:
+
+```
+$ mix test
+$ mix test.watch
+```
+
---
+++ lib/bottles.ex
@@ -0,0 +1,45 @@
+defmodule Bottles do
+  @moduledoc """
+  Documentation for `Bottles`.
+  """
+
+  def verse(0) do
+    """
+    No more bottles of beer on the wall, no more bottles of beer.
+    Go to the store and buy some more, 99 bottles of beer on the wall.
+    """
+  end
+
+  def verse(1) do
+    """
+    1 bottle of beer on the wall, 1 bottle of beer.
+    Take it down and pass it around, no more bottles of beer on the wall.
+    """
+  end
+
+  def verse(2) do
+    """
+    2 bottles of beer on the wall, 2 bottles of beer.
+    Take one down and pass it around, 1 bottle of beer on the wall.
+    """
+  end
+
+  @spec verse(integer()) :: String.t()
+  def verse(number) do
+    """
+    #{number} bottles of beer on the wall, #{number} bottles of beer.
+    Take one down and pass it around, #{number - 1} bottles of beer on the wall.
+    """
+  end
+
+  @spec song() :: String.t()
+  def song do
+    verses(99, 0)
+  end
+
+  @spec verses(integer(), integer()) :: String.t()
+  def verses(n, m) do
+    Enum.map(n..m, &verse/1)
+    |> Enum.join("\n")
+  end
+end
