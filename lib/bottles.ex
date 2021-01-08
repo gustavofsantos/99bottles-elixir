84f8df050b1a0e4483324a53fcb4818a02a3c85c
Gustavo Santos <gustavofsantos@outlook.com>
2021-01-08 16:08:43 -0300

    replace the default usage of bottles to the container function call

--- lib/bottles.ex
+++ lib/bottles.ex
@@ -28,7 +28,7 @@ defmodule Bottles do
   def verse(number) do
     """
     #{number} bottles of beer on the wall, #{number} bottles of beer.
-    Take one down and pass it around, #{number - 1} bottles of beer on the wall.
+    Take one down and pass it around, #{number - 1} #{container()} of beer on the wall.
     """
   end
 
@@ -43,7 +43,7 @@ defmodule Bottles do
     |> Enum.join("\n")
   end
 
-  defp container(_) do
+  defp container do
     "bottles"
   end
 end
