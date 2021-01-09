47b54d37de993fb8865380b1cc5ea855101d04ca
Gustavo Santos <gustavofsantos@outlook.com>
2021-01-09 12:13:06 -0300

    use the successor funciton at the default case

--- lib/bottles.ex
+++ lib/bottles.ex
@@ -14,7 +14,7 @@ defmodule Bottles do
   def verse(number) do
     """
     #{String.capitalize(quantity(number))} #{container(number)} of beer on the wall, #{quantity(number)} #{container(number)} of beer.
-    #{action(number)}, #{quantity(number - 1)} #{container(number - 1)} of beer on the wall.
+    #{action(number)}, #{quantity(successor(number))} #{container(number - 1)} of beer on the wall.
     """
   end
 
